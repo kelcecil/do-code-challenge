fc85eb435456140b96a1f15b4d8962e679ae4f8b
Kel Cecil <kelcecil@praisechaos.com>
2016-03-26 16:11:52 -0400

    Add initial working TCP server
    This is not complete. Needs unit tests and refactoring, but
    gives me better vision of what is needed.

---
+++ index_server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+)
+
+var (
+	packages *PackageSet = NewPackageSet()
+)
+
+func StartServer() {
+	log.Print("Starting server")
+	msgRouter := make(chan *Message, 0)
+	go MessageRouter(msgRouter)
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		connection, err := listener.Accept()
+		log.Print("Accepted connection")
+		if err != nil {
+			panic(err)
+		}
+		go HandleConnection(connection, msgRouter)
+	}
+}
+
+func HandleConnection(conn net.Conn, msgRouter chan<- *Message) {
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			log.Print("Connection closed")
+			conn.Close()
+			return
+		} else if err != nil {
+			log.Print(err)
+		}
+		message, _ := ParseMessage(line)
+		msgRouter <- message
+		log.Print("Reading response")
+		response := <-message.Response
+		log.Print(response)
+		_, err = conn.Write([]byte(response + "\n"))
+		if err != nil {
+			log.Print(err)
+		}
+	}
+}
