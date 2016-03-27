279b56dbd3fd799b7cc5aa5339449b9030faf5c0
Kel Cecil <kelcecil@praisechaos.com>
2016-03-27 17:47:07 -0400

    Add more MessageReader test cases

--- test/message_reader_test.go
+++ test/message_reader_test.go
@@ -24,25 +24,44 @@ func (mcr MockConnectionReader) Read(b []byte) (n int, err error) {
 	return mcr.testFunc(b)
 }
 
+func TestMessageReaderIndexDependencies(t *testing.T) {
+	msg := message.NewMessage("INDEX", "golang", []string{"glide", "yaml"})
+	testMessageReader(t, "INDEX|golang|glide,yaml\n", msg)
+}
+
 func TestMessageReaderIndex(t *testing.T) {
-	index := func(b []byte) (n int, err error) {
-		result := []byte("INDEX|golang|\n")
+	msg := message.NewMessage("INDEX", "golang", []string{})
+	testMessageReader(t, "INDEX|golang|\n", msg)
+}
+
+func TestMessageReaderRemove(t *testing.T) {
+	msg := message.NewMessage("REMOVE", "golang", []string{})
+	testMessageReader(t, "REMOVE|golang|\n", msg)
+}
+
+func TestMessageReaderQuery(t *testing.T) {
+	msg := message.NewMessage("QUERY", "golang", []string{})
+	testMessageReader(t, "QUERY|golang|\n", msg)
+}
+
+func testMessageReader(t *testing.T, query string, expected *message.Message) {
+	call := func(b []byte) (n int, err error) {
+		result := []byte(query)
 		for i := range result {
 			b[i] = result[i]
 		}
 		return len(result), nil
 	}
 
-	mcr := NewMockConnectionReader(index)
+	mcr := NewMockConnectionReader(call)
 	reader := parser.NewMessageReader(mcr)
 
 	msg, err := reader.Read()
 	if err != nil {
 		t.Errorf("Reader returned an error; Error: %v", err)
 	}
-	expectedMessage := message.NewMessage("INDEX", "golang", []string{})
 
-	if !msg.Equals(expectedMessage) {
-		t.Errorf("Messages did not match; Wanted: %v, Got: %v", expectedMessage.String(), msg.String())
+	if !msg.Equals(expected) {
+		t.Errorf("Messages did not match; Wanted: %v, Got: %v", expected.String(), msg.String())
 	}
 }
